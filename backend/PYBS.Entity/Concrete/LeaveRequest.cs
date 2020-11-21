c39efa99d80247787c6d8cb950df1f502f5b8802
lockette <alperenerdogan0505@gmail.com>
2020-11-21 11:01:29 +0300

    a

---
+++ backend/PYBS.Entity/Concrete/LeaveRequest.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PYBS.Entity.Concrete
+{
+  public class LeaveRequest
+  {
+  }
+}
---
+++ backend/PYBS.Entity/Concrete/LeaveType.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PYBS.Entity.Concrete
+{
+  public class LeaveType
+  {
+    public int Id { get; set; }
+    public string LeaveName { get; set; }
+    public bool IsPaid { get; set; }
+  }
+}
