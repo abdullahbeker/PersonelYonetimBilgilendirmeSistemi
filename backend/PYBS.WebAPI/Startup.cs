9864c9fb8247db23f5951d979ef9070732b58207
Abdullah Beker <abdullahbeker.54@hotmail.com>
2020-11-22 23:35:12 +0300

    add cors support

--- backend/PYBS.WebAPI/Startup.cs
+++ backend/PYBS.WebAPI/Startup.cs
@@ -47,6 +47,7 @@ namespace PYBS.WebAPI
                 };
             });
             services.AddControllers();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +63,7 @@ namespace PYBS.WebAPI
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
