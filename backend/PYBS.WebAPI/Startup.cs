a7636dd4f2717992c04c098bbf6a4059cec23a9e
Abdullah Beker <abdullahbeker.54@hotmail.com>
2021-01-13 16:41:36 +0300

    add developer exception page

--- backend/PYBS.WebAPI/Startup.cs
+++ backend/PYBS.WebAPI/Startup.cs
@@ -53,10 +53,7 @@ namespace PYBS.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            app.UseDeveloperExceptionPage();
             app.UseExceptionHandler("/Error");
             app.UseHttpsRedirection();
 
