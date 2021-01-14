1ed8bf93f43be066f4740a8deceaba8e74583a6a
Seyyit <seyyitatim@gmail.com>
2021-01-14 18:23:57 +0300

    trainingmap fixed

--- backend/PYBS.DataAccess/Concrete/EntityFrameworkCore/Mapping/TrainingMaps/TrainingMap.cs
+++ backend/PYBS.DataAccess/Concrete/EntityFrameworkCore/Mapping/TrainingMaps/TrainingMap.cs
@@ -21,6 +21,8 @@ namespace PYBS.DataAccess.Concrete.EntityFrameworkCore.Mapping.TrainingMaps
             builder.Property(x => x.Location).HasMaxLength(50).IsRequired();
             builder.Property(x => x.EducatingFirm).HasMaxLength(50).IsRequired();
             builder.Property(x => x.Instructor).HasMaxLength(50).IsRequired();
+
+            builder.HasMany(x => x.TrainingPersonnels).WithOne(x => x.Training).HasForeignKey(x => x.TrainingId).OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
--- backend/PYBS.DataAccess/Migrations/20210114152329_mig40.Designer.cs
+++ backend/PYBS.DataAccess/Migrations/20210114152329_mig40.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PYBS.DataAccess.Concrete.EntityFrameworkCore.Context;
 
 namespace PYBS.DataAccess.Migrations
 {
     [DbContext(typeof(PYBSContext))]
-    partial class PYBSContextModelSnapshot : ModelSnapshot
+    [Migration("20210114152329_mig40")]
+    partial class mig40
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
